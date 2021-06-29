dfb21dcdb0dbbefe7b7cce61f2353a6ef02767e6
fegiacomelli <69607844+fegiacomelli@users.noreply.github.com>
2021-06-29 08:01:33 -0300

    Att 1.0.2

--- blogPessoal/src/main/java/com/generation/blogPessoal/Controller/PostagemController.java
+++ blogPessoal/src/main/java/com/generation/blogPessoal/Controller/PostagemController.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -47,8 +48,16 @@ public class PostagemController {
 	}
 	
 	@PutMapping
-	public ResponseEntity<Postagem> put (@RequestBody Postagem postagem){
+	public ResponseEntity<Postagem> postarput (@RequestBody Postagem postagem){
 		return ResponseEntity.status(HttpStatus.OK).body(repository.save(postagem));
 	}
+	
+	@DeleteMapping("/{id}")
+	public void deletar (@PathVariable long id) {
+		repository.deleteById(id);
+	}
+	
+	
+	
 
 }
